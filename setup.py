d95ad5c6ec67251053c0f6028af3ea4cc528688f
Guillaume Sousa Amaral <guillaume.sousa@nist.gov>
2019-03-14 22:18:43 -0400

    fix: core 1.0.1 sets kombu version to avoid broken celery dependency
    Change-Id: I359f89d05337f90d8832fb5db54ac62cbaad36ad

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ chdir(normpath(join(abspath(__file__), pardir)))
 
 setup(
     name="core_module_status_registry_app",
-    version="1.0.0",
+    version="1.0.1",
     description="Status registry module",
     long_description=long_desc,
     author="NIST IT Lab",
