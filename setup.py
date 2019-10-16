cf624d836b67d5c8435bda92d9760240fed51546
Augustin CHINI <augustin.chini@nist.gov>
2019-10-16 13:59:39 -0400

    chore: upgrade 1.6.0
    Change-Id: I766d0a01d9cbc262d37be1e9d0fb3031ec902995

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ chdir(normpath(join(abspath(__file__), pardir)))
 
 setup(
     name="core_module_status_registry_app",
-    version="1.5.0",
+    version="1.6.0",
     description="Status registry module",
     long_description=long_desc,
     author="NIST IT Lab",
