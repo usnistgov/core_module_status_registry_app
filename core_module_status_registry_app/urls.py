831426796f1148bce1110e7f3a48a449be75cc5d
Guillaume Sousa Amaral <guillaume.sousa@nist.gov>
2019-10-24 14:50:36 -0400

    fix: revert django upgrade
    Change-Id: Iaad7f38d60b73e92ffc6766fe152e51c6f08336d

--- core_module_status_registry_app/urls.py
+++ core_module_status_registry_app/urls.py
@@ -1,10 +1,9 @@
 """ Url router for the Status registry module
 """
-
-from django.urls import re_path
+from django.conf.urls import url
 
 from core_module_status_registry_app.views.views import StatusRegistryModule
 
 urlpatterns = [
-    re_path(r'module-status-registry', StatusRegistryModule.as_view(), name='core_module_status_registry_view'),
+    url(r'module-status-registry', StatusRegistryModule.as_view(), name='core_module_status_registry_view'),
 ]
