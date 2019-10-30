7b8df55cf065fc6524add44b133f511e9a7eb873
Guillaume Sousa Amaral <guillaume.sousa@nist.gov>
2019-10-30 10:16:38 -0400

    feat: upgrade django 2.2
    Change-Id: I836064d09a1cedcab081e72456a8bc068701ca8e

--- core_module_status_registry_app/urls.py
+++ core_module_status_registry_app/urls.py
@@ -1,9 +1,10 @@
 """ Url router for the Status registry module
 """
-from django.conf.urls import url
+
+from django.urls import re_path
 
 from core_module_status_registry_app.views.views import StatusRegistryModule
 
 urlpatterns = [
-    url(r'module-status-registry', StatusRegistryModule.as_view(), name='core_module_status_registry_view'),
+    re_path(r'module-status-registry', StatusRegistryModule.as_view(), name='core_module_status_registry_view'),
 ]
