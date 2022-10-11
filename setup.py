e8d329d9cde1740e291b35d541472573b4fd5d2e
Guillaume Sousa <guillaume.sousa@nist.gov>
2022-10-11 20:42:27 +0000

    chore: upgrade 2.1.0
    Change-Id: I1cb8652768a7df73d707fbb3325ddc030abf0d44

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ chdir(normpath(join(abspath(__file__), pardir)))
 
 setup(
     name="core_module_status_registry_app",
-    version="2.0.0",
+    version="2.1.0",
     description="Status registry module",
     long_description=long_desc,
     author="NIST IT Lab",
